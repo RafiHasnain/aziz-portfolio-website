44685640e2519eb0754cb780f55a1dc923e5b262
Rafi Hasnain <rafihasnain02@gmail.com>
2025-04-19 20:36:21 +0600

    Resume has been changed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     // Create a link element
     const link = document.createElement("a");
     link.href = "/resume.pdf"; // Path to your PDF in the public folder
-    link.download = "Resume of Aziz.pdf"; // Name for the downloaded file
+    link.download = "Resume of Azizul Islam.pdf"; // Name for the downloaded file
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
